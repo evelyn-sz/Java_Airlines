d1232e18f8e6d6514afc02fbd343d0155463c8f1
evelyn-sz <evelynszarek@gmail.com>
2021-02-14 17:39:02 +0000

    Flight has flightNumber, destinationAirport, departureAirport and departureTime

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,29 @@
 public class Flight {
+    private String flightNumber;
+    private String destinationAirport;
+    private String departureAirport;
+    private String departureTime;
+
+    public Flight(String flightNumber, AirportsList destinationAirport, AirportsList departureAirport, String departureTime) {
+        this.flightNumber = flightNumber;
+        this.destinationAirport = destinationAirport.getAirport();
+        this.departureAirport = departureAirport.getAirport();
+        this.departureTime = departureTime;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestinationAirport() {
+        return destinationAirport;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,14 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
 
     Flight flight;
     Pilot yoda;
-    Pilot obiWan;
-    CabinCrewMember anakin;
-    CabinCrewMember chewbacca;
+    Pilot chewbacca;
+    CabinCrewMember r2d2;
+    CabinCrewMember k2so;
     CabinCrewMember c3po;
-    Passenger r2d2;
+    Passenger anakin;
     Passenger luke;
     Passenger darthVader;
     Plane marsRover;
 
+    @Before
+    public void setUp(){
+        flight = new Flight("POI098", AirportsList.LHR, AirportsList.EDI, "06:00");
+    }
+
+    @Test
+    public void flightHasNumber(){
+        assertEquals("POI098", flight.getFlightNumber());
+    }
+
+    @Test
+    public void hasDestinationAirport() {
+        assertEquals("London Heathrow", flight.getDestinationAirport());
+    }
+
+    @Test
+    public void hasDepartureAirport() {
+        assertEquals("Edinburgh", flight.getDepartureAirport());
+    }
+
+    @Test
+    public void hasDepartureTime() {
+        assertEquals("06:00", flight.getDepartureTime());
+    }
 }
