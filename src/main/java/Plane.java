ab9863352abf71a8a54c83e41f6f6fd0bc6f4be3
evelyn-sz <evelynszarek@gmail.com>
2021-02-14 16:55:17 +0000

    Plane tests passed: model, passengerCapacity and baggageCapacity

--- src/main/java/Plane.java
+++ src/main/java/Plane.java
@@ -1,2 +1,23 @@
 public class Plane {
+    private String model;
+    private int passengerCapacity;
+    private int baggageCapacity;
+
+    public Plane(String model, int passengerCapacity, int baggageCapacity) {
+        this.model = model;
+        this.passengerCapacity = passengerCapacity;
+        this.baggageCapacity = baggageCapacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPassengerCapacity() {
+        return passengerCapacity;
+    }
+
+    public int getBaggageCapacity() {
+        return baggageCapacity;
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,4 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
 
     Plane plane;
+
+    @Before
+    public void setUp(){
+        plane = new Plane("Mars Rover", 2, 4);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Mars Rover", plane.getModel());
+    }
+
+    @Test
+    public void hasPassengerCapacity() {
+        assertEquals(2, plane.getPassengerCapacity());
+    }
+
+    @Test
+    public void hasBaggageCapacity() {
+        assertEquals(4, plane.getBaggageCapacity());
+    }
 }
