78b004ac4eea6ec91aa32f72bfef41717cc3c3c3
evelyn-sz <evelynszarek@gmail.com>
2021-02-14 20:07:16 +0000

    Pilot, Passenger and CabinCrewMember lists start empty

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,14 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Flight {
     private String flightNumber;
     private String destinationAirport;
     private String departureAirport;
     private String departureTime;
+    private ArrayList<Pilot> pilotList;
+    private ArrayList<CabinCrewMember> cabinCrewMemberList;
+    private ArrayList<Passenger> passengerList;
+    private Object assignedPlane;
 
     public Flight(String flightNumber, AirportsList destinationAirport, AirportsList departureAirport, String departureTime) {
         this.flightNumber = flightNumber;
         this.destinationAirport = destinationAirport.getAirport();
         this.departureAirport = departureAirport.getAirport();
         this.departureTime = departureTime;
+        this.pilotList = new ArrayList<Pilot>();
+        this.cabinCrewMemberList = new ArrayList<CabinCrewMember>();
+        this.passengerList = new ArrayList<Passenger>();
+        this.assignedPlane = assignedPlane;
     }
 
     public String getFlightNumber() {
@@ -25,5 +36,25 @@ public class Flight {
 
     public String getDepartureTime() {
         return departureTime;
+    }
+
+    public ArrayList getListOfCabinCrewMembers() {
+        return cabinCrewMemberList;
+    }
+
+    public ArrayList getListOfPassengers() {
+        return passengerList;
+    }
+
+    public ArrayList getListOfPilots(){
+        return this.pilotList;
+
+    }
+    public void add(ArrayList array, Object object){
+        array.add(object);
+    }
+
+    public int getListSize(ArrayList array) {
+        return array.size();
     }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,6 +1,8 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
@@ -15,10 +17,26 @@ public class FlightTest {
     Passenger luke;
     Passenger darthVader;
     Plane marsRover;
+    ArrayList pilotList;
+    ArrayList cabinCrewMemberList;
+    ArrayList passengerList;
 
     @Before
     public void setUp(){
-        flight = new Flight("POI098", AirportsList.LHR, AirportsList.EDI, "06:00");
+        flight  = new Flight("POI098", AirportsList.LHR, AirportsList.EDI, "06:00");
+        yoda    = new Pilot("Yoda", CrewMemberRank.PILOT, "ABC123" );
+        chewbacca = new Pilot("Chewbacca", CrewMemberRank.PILOT, "DEF123");
+        r2d2 = new CabinCrewMember("R2D2", CrewMemberRank.FLIGHTMANAGER);
+        k2so = new CabinCrewMember("K2SO", CrewMemberRank.FLIGHTATTENDANT);
+        c3po = new CabinCrewMember("C3PO", CrewMemberRank.FLIGHTATTENDANT);
+        anakin  = new Passenger("Anakin", 1);
+        luke    = new Passenger("Luke", 2);
+        darthVader  = new Passenger("Darth Vader", 4);
+        marsRover   = new Plane("Mars Rover", 2, 2);
+        pilotList           = new ArrayList<Pilot>(flight.getListOfPilots());
+        cabinCrewMemberList = new ArrayList<CabinCrewMember>(flight.getListOfCabinCrewMembers());
+        passengerList       = new ArrayList<Passenger>(flight.getListOfPassengers());
+
     }
 
     @Test
@@ -40,4 +58,37 @@ public class FlightTest {
     public void hasDepartureTime() {
         assertEquals("06:00", flight.getDepartureTime());
     }
+
+    @Test
+    public void pilotListStartsEmpty() {
+        assertEquals(0, flight.getListSize(pilotList));
+    }
+    @Test
+    public void canAddPilots() {
+        flight.add(pilotList, yoda);
+        flight.add(pilotList, chewbacca);
+        assertEquals(2, flight.getListSize(pilotList));
+    }
+
+    @Test
+    public void crewMemberListStartsEmpty() {
+        assertEquals(0, flight.getListSize(cabinCrewMemberList));
+    }
+//
+//    @Test
+//    public void canAddCrewMembers() {
+//    }
+//
+    @Test
+    public void passengerListStartsEmpty() {
+        assertEquals(0, flight.getListSize(passengerList));
+    }
+//
+//    @Test
+//    public void canAddPassengers() {
+//    }
+//
+//    @Test
+//    public void hasPlaneAssigned() {
+//    }
 }
