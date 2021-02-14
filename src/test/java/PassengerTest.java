8d94176eb29916c1eec39e3157a554ec57a4f980
evelyn-sz <evelynszarek@gmail.com>
2021-02-14 16:32:23 +0000

    PassengerTest hasName and hasNumberOfBags tests passed

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,3 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
-    Passenger passenger;
+    Passenger luke;
+
+    @Before
+    public void setUp(){
+        luke = new Passenger("Luke", 1);
+    }
+
+    @Test
+    public void passengerHasName(){
+        assertEquals("Luke", luke.getName());
+    }
+
+    @Test
+    public void passengerHasLuggage(){
+        assertEquals(1, luke.getNumberOfBags());
+    }
 }
