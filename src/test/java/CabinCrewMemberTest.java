fe59ec7760e4c6e216722c8f4237dde1a19912b1
evelyn-sz <evelynszarek@gmail.com>
2021-02-14 15:59:28 +0000

    CabinCrewMember has name and can talk to passengers

--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,3 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewMemberTest {
-    CabinCrewMember cabinCrewMember;
+    CabinCrewMember anakin;
+
+    @Before
+    public void setUp(){
+        anakin = new CabinCrewMember("Anakin", CrewMemberRank.FLIGHTATTENDANT);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Anakin", anakin.getName());
+    }
+
+    @Test
+    public void canTalkToPassengers(){
+        assertEquals("We're about to go past The Death Star.", anakin.talk());
+    }
 }
